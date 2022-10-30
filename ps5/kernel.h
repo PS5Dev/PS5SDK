3d5788ff3a9b7f9d1eec87804a9db6e2b30e6b6d
SpecterDev <cryptogenicdev@gmail.com>
2022-10-30 15:10:45 -0400

    Add kernel offsets for other firmwares

--- ps5/kernel.h
+++ ps5/kernel.h
@@ -12,8 +12,20 @@
 #include <stddef.h>
 
 // Firmware includes
-#if PS5_FW_VERSION == 0x403
+#if PS5_FW_VERSION == 0x300
+#include <ps5/kernel_offsets/offsets_300.h>
+#elif PS5_FW_VERSION == 0x320
+#include <ps5/kernel_offsets/offsets_320.h>
+#elif PS5_FW_VERSION == 0x321
+#include <ps5/kernel_offsets/offsets_321.h>
+#elif PS5_FW_VERSION == 0x402
+#include <ps5/kernel_offsets/offsets_402.h>
+#elif PS5_FW_VERSION == 0x403
 #include <ps5/kernel_offsets/offsets_403.h>
+#elif PS5_FW_VERSION == 0x450
+#include <ps5/kernel_offsets/offsets_450.h>
+#elif PS5_FW_VERSION == 0x451
+#include <ps5/kernel_offsets/offsets_451.h>
 #else
 #error SDK does not support this kernel version for kernel hacking.
 #endif // PS5_FW_VERSION
---
+++ ps5/kernel_offsets/offsets_321.h
@@ -0,0 +1,22 @@
+/*****************************************************
+ * PS5 SDK - 3.21 Kernel Offsets
+ * Contains offsets for 3.21.
+ ****************************************************/
+
+#ifndef PS5SDK_KERNEL_OFFSETS_321_H
+#define PS5SDK_KERNEL_OFFSETS_321_H
+
+// Proc field offsets
+#define OFFSET_KERNEL_PROC_P_UCRED          0x40
+#define OFFSET_KERNEL_PROC_P_PID            0xBC
+
+// Ucred field offsets
+#define OFFSET_KERNEL_UCRED_CR_UID          0x04
+#define OFFSET_KERNEL_UCRED_CR_RUID         0x08
+#define OFFSET_KERNEL_UCRED_CR_SVUID        0x0C
+#define OFFSET_KERNEL_UCRED_CR_RGID         0x14
+
+// Offsets from kernel .data base
+#define OFFSET_KERNEL_DATA_BASE_ALLPROC     0x276DC58
+
+#endif // PS5SDK_KERNEL_OFFSETS_321_H
