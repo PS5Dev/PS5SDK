8057907480353f2eae839d3c13fd2420e4d18fb9
John Törnblom <john.tornblom@gmail.com>
2023-02-10 18:43:25 +0100

    compile statically, with proper offsets to .text and .data

--- cmake/toolchain-ps5.cmake
+++ cmake/toolchain-ps5.cmake
@@ -36,15 +36,9 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)# Search for programs in the build h
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)	# For libraries and headers in the target directories
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(CMAKE_C_FLAGS "--target=x86_64-freebsd-pc-elf -O0 -DPPR -DPS5 -DPS5_FW_VERSION=${V_FW} ") # -D_KERNEL=1 x86_64-scei-ps5-elf
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_POSIX_SOURCE -D_POSIX_C_SOURCE=200112 -D__BSD_VISIBLE=1 -D__XSI_VISIBLE=500")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-builtin -nostdlib -Wall -m64") #  -nostartfiles
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfsgsbase -maes -mavx2 -mbmi -march=znver2")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-omit-frame-pointer -fPIC -mcmodel=small -nostdinc -nodefaultlibs")
-
+set(CMAKE_ASM_FLAGS "-ffreestanding -fno-builtin -nostdlib -static")
+set(CMAKE_C_FLAGS "${CMAKE_ASM_FLAGS} -DPPR -DPS5 -DPS5_FW_VERSION=${V_FW} -D_POSIX_SOURCE -D_POSIX_C_SOURCE=200112 -D__BSD_VISIBLE=1 -D__XSI_VISIBLE=500")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -nostdinc++ ")
 
-set(CMAKE_ASM_FLAGS "--target=x86_64-freebsd-pc-elf -nostdlib -fPIC")
-
 #-DCMAKE_LINKER=/path/to/linker -DCMAKE_CXX_LINK_EXECUTABLE="<CMAKE_LINKER> <FLAGS> <CMAKE_CXX_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>"
-set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld -Xlinker -T ${CMAKE_CURRENT_SOURCE_DIR}/linker.x -Wl,--build-id=none")
+set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld -static -Xlinker -T ${CMAKE_CURRENT_SOURCE_DIR}/linker.x -Wl,--build-id=none -Wl,-Ttext=0x920100000 -Wl,-Tdata=0x926100000")
