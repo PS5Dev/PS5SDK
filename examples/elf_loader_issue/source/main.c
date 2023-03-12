b0a94f6222097ec73c3d5017362f4d09dc497113
John Törnblom <john.tornblom@gmail.com>
2023-03-12 16:15:44 +0100

    add example where forked procs cannot use dlsym.

--- examples/elf_loader_issue/source/main.c
+++ examples/elf_loader_issue/source/main.c
@@ -1,16 +1,62 @@
-// Required header
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <ps5/payload_main.h>
 
 
-static char* strings[] = {
-  "ABCD",
-  "EFGH",
-  "IJKL",
-  "MNOP"
-};
+typedef struct notify_request {
+  char useless1[45];
+  char message[3075];
+} notify_request_t;
 
 
-int payload_main(struct payload_args *args)
-{
-	return strings[2][2];
+int sceKernelSendNotificationRequest(int, notify_request_t*, size_t, int);
+
+
+static void notify(const char *s) {
+  notify_request_t req;
+
+  bzero(&req, sizeof req);
+  strncpy(req.message, s, sizeof req.message);
+
+  sceKernelSendNotificationRequest(0, &req, sizeof req, 0);
+}
+
+
+static int
+main(void *args) {
+  char buf[255];
+  struct payload_args *payload_args = args;
+
+  void* (*f_malloc)(size_t size) = 0;
+  int ret = payload_args->dlsym(0x2, "malloc", &f_malloc);
+  
+  sleep(5);
+  
+  sprintf(buf, "%x\n", ret);
+  notify(buf);
+
+  while(1) {
+    sleep(1);
+  }
+  
+  return ret;
+}
+
+
+int payload_main(struct payload_args *args) {
+  void* (*f_malloc)(size_t size) = 0;
+  int ret = args->dlsym(0x2, "malloc", &f_malloc);
+  if(ret || !f_malloc) {
+    return ret;
+  }
+  
+  sleep(5);
+  notify("Forking...\n");
+  if (rfork_thread(RFPROC | RFFDG, f_malloc(0x4000), main, args) < 0) {
+    return EXIT_FAILURE;
+  } else {
+    return EXIT_SUCCESS;
+  }
 }
