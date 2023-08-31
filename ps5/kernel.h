c38805685ae06b0dfe42ec941bc5c3ed5ef0fbc0
John Törnblom <john.tornblom@gmail.com>
2023-08-31 23:13:41 +0200

    add some error reporting to kernel rw primitves

--- ps5/kernel.h
+++ ps5/kernel.h
@@ -32,7 +32,7 @@
 
 // Public hacking API
 void kernel_init_rw(int master_sock, int victim_sock, int *rw_pipe, uint64_t pipe_addr);
-void kernel_copyin(void *src, uint64_t kdest, size_t length);
-void kernel_copyout(uint64_t ksrc, void *dest, size_t length);
+int kernel_copyin(void *src, uint64_t kdest, size_t length);
+int kernel_copyout(uint64_t ksrc, void *dest, size_t length);
 
 #endif // PS5SDK_KERNEL_H
